9843953e3fe7fbd359cff6651077d249ea2c0945
zulpukarov02 <saifidinovbekturgan@gmail.com>
2023-05-23 17:19:52 +0600

    create arhitecture

---
+++ lib/add_product/add_product.dart
@@ -0,0 +1,1 @@
+export 'page/add_product_page.dart';
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:jarnama_exzemple/main.dart';
+import 'package:jarnama_exzemple/home/page/home_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HomePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
